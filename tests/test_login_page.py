1a93cb685e7970b083107123b42e509322f3cfa1
selfe <davit.mnatobishvili.1@btu.edu.ge>
2023-02-12 20:25:41 +0400

    Added new test case #001

--- tests/test_login_page.py
+++ tests/test_login_page.py
@@ -24,3 +24,12 @@ def test_main_page_negative(main_page):
     expected_text = "Epic sadface: Username and password do not match any user in this service"
     actual_text = main_page.find_element(LoginPageLocators.ERROR_MESSAGE).text
     assert actual_text == expected_text
+
+
+def test_main_page_negative_username(main_page):
+    main_page.input_user('araswori_username')
+    main_page.input_password('secret_sauce')
+    main_page.click_login()
+    expected_text = "Epic sadface: Username and password do not match any user in this service"
+    actual_text = main_page.find_element(LoginPageLocators.ERROR_MESSAGE).text
+    assert actual_text == expected_text
